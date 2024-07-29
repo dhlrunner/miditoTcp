72365d2b237ef91e161024ba3d3fb37a93322062
Dohyung Lee <dhlrunner@gmail.com>
2024-07-29 11:26:57 +0900

    Remove unnecessary code

--- miditoTcp/Program.cs
+++ miditoTcp/Program.cs
@@ -14,8 +14,6 @@ namespace miditoTcp
     class Program
     {
         static InputDevice[] InputDevices = null;
-        //static InputDevice inputDeviceA = null;
-        //static InputDevice inputDeviceB = null;
 
         static string ipAddr = "10.10.4.23";
         static int port = 12345;
@@ -35,7 +33,6 @@ namespace miditoTcp
             ("MMT-TG D", 5)
         };
 
-        //static byte PortOffset = 1;
         static void Main(string[] args)
         {
             //byte[] midiDataBuffer = new byte[128];
@@ -54,7 +51,7 @@ namespace miditoTcp
                     List<byte> data = x.Convert(e.Event).ToList();
                     if(data.Count > 0)
                     {
-
+                        
                         if (data[0] == 0xf0)
                         {
                             //byte[] newdata = new byte[(data.Count - 1) + 3];
@@ -105,36 +102,16 @@ namespace miditoTcp
                         Console.WriteLine("Error: Empty data");
                     }
 
-                    //data.InsertRange(0, new byte[] { 0xF5, (byte)(portIndex + 1 + PortOffset) });
-                    //data.Insert(0, (byte)data.Count);
-                    //tcp.Send(data.ToArray());
                 };
                 InputDevices[i].StartEventsListening();
                 Console.WriteLine($"Opened MIDI Device {InputDevices[i].Name} ({InputDevices[i].Id}) Port: {inputDev[i].portNum}");
             }
 
-            //inputDeviceA = InputDevice.GetByName("SAM5916 A");
-            //inputDeviceA.EventReceived += OnEventReceived1;
-            //inputDeviceA.StartEventsListening();
-
-            //inputDeviceB = InputDevice.GetByName("SAM5916 B");
-            //inputDeviceB.EventReceived += OnEventReceived2;
-            //inputDeviceB.StartEventsListening();
-
-            //tcpClient = new TcpClient(ipAddr, port);
-            //tcpClient.NoDelay = true;
-
-            //stream = tcpClient.GetStream();
-
             tcp = new TcpCommunicator(ipAddr, port);
 
             Console.WriteLine($"Connected to {ipAddr}:{port}");
             Console.WriteLine("Press enter to exit");
-            //TcpListener server = new TcpListener(IPAddress.Any, 9999);
-            //server.Start();
             Console.ReadLine();
-            //inputDeviceA.Dispose();
-            //inputDeviceB.Dispose();
 
             for (int i = 0; i < inputDev.Length; i++) {
                 InputDevices[i].Dispose();
@@ -143,59 +120,6 @@ namespace miditoTcp
             tcp.Dispose();
             
            
-        }
-        private static void OnEventReceived1(object sender, MidiEventReceivedEventArgs e)
-        {
-            var midiDevice = (MidiDevice)sender;
-            
-            MidiEventToBytesConverter x = new MidiEventToBytesConverter();
-            List<byte> data = x.Convert(e.Event).ToList();
-            //Console.WriteLine("Midi A data in ");
-            //foreach(byte a in data)
-            //{
-            //    Console.Write("{0:X2} ", a);
-            //}
-            //Console.WriteLine();
-            
-            if (data[0] == 0xf0)
-            {
-                data.RemoveAt(1);              
-            }
-
-            data.InsertRange(0, new byte[] {0xF5,0x02 });
-            tcp.Send(data.ToArray());
-            //stream.Write(data.ToArray(), 0, data.Count);
-            
-
-            //SendTcp(socket, data, 0, data.Length, 10000);
-
-        }
-        private static void OnEventReceived2(object sender, MidiEventReceivedEventArgs e)
-        {
-            var midiDevice = (MidiDevice)sender;
-
-            MidiEventToBytesConverter x = new MidiEventToBytesConverter();
-            List<byte> data = x.Convert(e.Event).ToList();
-            //Console.WriteLine("Midi B data in ");
-            //foreach (byte a in data)
-            //{
-            //    Console.Write("{0:X2} ", a);
-            //}
-            //Console.WriteLine();
-
-            if (data[0] == 0xf0)
-            {
-                data.RemoveAt(1);
-            }
-
-            data.InsertRange(0, new byte[] { 0xF5, 0x03 });
-            tcp.Send(data.ToArray());
-            //stream.Write(data.ToArray(), 0, data.Count);
-            //stream.Close();
-            //tcpClient.Close();
-
-            //SendTcp(socket, data, 0, data.Length, 10000);
-
         }
 
         static void CurrentDomain_ProcessExit(object sender, EventArgs e)
