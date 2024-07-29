40b886144b1744ab1fd7a1f5c4669e0f872778b3
Dohyung Lee <dhlrunner@gmail.com>
2024-07-29 18:29:40 +0900

    퍼포먼스 향상 (List 대신 배열 new 사용하도록 변경)

--- miditoTcp/Program.cs
+++ miditoTcp/Program.cs
@@ -15,7 +15,9 @@ namespace miditoTcp
     {
         static InputDevice[] InputDevices = null;
 
-        static string ipAddr = "10.10.4.23";
+        //172.27.11.136
+        //10.10.4.23
+        static string ipAddr = "172.27.11.136";
         static int port = 12345;
 
         static TcpClient tcpClient = null;
@@ -47,44 +49,49 @@ namespace miditoTcp
                     var midiDevice = (MidiDevice)sender;
                     //미디 데이터 수신할때마다 new 해주지 않으면 데이터가 이상하게 변환됨
                     MidiEventToBytesConverter x = new MidiEventToBytesConverter();
-                    //byte[] data = x.Convert(e.Event);
-                    List<byte> data = x.Convert(e.Event).ToList();
-                    if(data.Count > 0)
+                    
+                    byte[] data = x.Convert(e.Event);
+                    //List<byte> data = x.Convert(e.Event).ToList();
+                    if(data.Length > 0)
                     {
-                        
+                        byte[] newdata = null; 
                         if (data[0] == 0xf0)
                         {
-                            //byte[] newdata = new byte[(data.Count - 1) + 3];
-
-                            //newdata[0] = (byte)((data.Count - 1) + 2); //length
-                            //newdata[1] = 0xF5;
-                            //newdata[2] = (byte)(inputDev[portIndex].portNum);
-                            //newdata[3] = 0xF0;
-                            //for (int j = 2; j < data.Count; j++)
-                            //{
-                            //    newdata[j + 2] = data[j];
-                            //}
-                            data.RemoveAt(1);
-                            data.InsertRange(0, new byte[] { 0xFF, (byte)(data.Count + 2), 0xF5, inputDev[portIndex].portNum });
+                            //SysEx일경우 -1
+                            //STX + Size + F5 + Port = 4
+                            newdata = new byte[(data.Length - 1) + 4];
+                            newdata[0] = 0xFF;
+                            newdata[1] = (byte)((data.Length - 1) + 2); //length
+                            newdata[2] = 0xF5;
+                            newdata[3] = inputDev[portIndex].portNum;
+                            newdata[4] = 0xF0;
+                            for (int j = 2; j < data.Length; j++)
+                            {
+                                newdata[j + 3] = data[j];
+                            }
+                            //data.RemoveAt(1);
+                            //data.InsertRange(0, new byte[] { 0xFF, (byte)(data.Count + 2), 0xF5, inputDev[portIndex].portNum });
                             
                         }
                         else
                         {
-                            //byte[] newdata = new byte[(data.Count) + 3];
-                            //newdata[0] = (byte)((data.Count) + 2); //length
-                            //newdata[1] = 0xF5;
-                            //newdata[2] = (byte)(inputDev[portIndex].portNum);
-                            //for (int j = 0; j < data.Count; j++)
-                            //{
-                            //    newdata[j + 3] = data[j];
-                            //}
-                            data.InsertRange(0, new byte[] { 0xFF, (byte)(data.Count + 2), 0xF5, inputDev[portIndex].portNum });
+                            //STX + Size + F5 + Port = 4
+                            newdata = new byte[(data.Length) + 4];
+                            newdata[0] = 0xFF;
+                            newdata[1] = (byte)(data.Length + 2); //length
+                            newdata[2] = 0xF5;
+                            newdata[3] = inputDev[portIndex].portNum;
+                            for (int j = 0; j < data.Length; j++)
+                            {
+                                newdata[j + 4] = data[j];
+                            }
+                            //data.InsertRange(0, new byte[] { 0xFF, (byte)(data.Count + 2), 0xF5, inputDev[portIndex].portNum });
                             //tcp.Send(data.ToArray());
                         }
 
                         try
                         {
-                            tcp.Send(data.ToArray());
+                            tcp.Send(newdata);
                         }
                         catch(InvalidOperationException)
                         {
